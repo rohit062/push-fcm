d62c4d200ad5cfdfbb2788a2f517fd69457e83f3
Rohit Salaria <rohit.salaria@jungleworks.com>
2018-12-22 20:10:42 +0530

    added error constants

---
+++ constants/errorConstants.js
@@ -0,0 +1,12 @@
+let errorMessages = {};
+
+// define javascript constants
+
+errorMessages.fcmError = {
+  invalidServerResponse : "InvalidServerResponse",
+  notAuthorizedError : "NotAuthorizedError"
+}
+
+module.exports = {
+  errorMessages
+}
